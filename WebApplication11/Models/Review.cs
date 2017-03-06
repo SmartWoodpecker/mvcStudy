817a32e12b64824f77bfddfaf8cccc411f253d19
SmartWoodpecker <smartrandle@gmail.com>
2017-03-06 17:06:25 +0300

    first commit

---
+++ WebApplication11/Models/Review.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication11.Models
+{
+    public class Review
+    {
+        public int ReviewID { get; set; }
+
+        [Display(Name = "Album")]
+        public int AlbumID { get; set; }
+        public virtual Album Album { get; set; }
+
+        public string Contents { get; set; }
+
+        [Required()]
+        [Display(Name = "Email Address")]
+        [DataType(DataType.EmailAddress)]
+        public string ReviewerEmail { get; set; }
+    }
+}
